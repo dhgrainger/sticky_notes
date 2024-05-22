b26b77a32c9cfa6688887f90e41f8aae46fa2ae1
lyndbergh-simelus-sb <lyndbergh.simelus@simplybusiness.com>
2024-05-22 10:30:06 -0400

    step 14

--- back_end/app/controllers/sticky_notes_controller.rb
+++ back_end/app/controllers/sticky_notes_controller.rb
@@ -1,2 +1,27 @@
 class StickyNotesController < ApplicationController
-end
+    def index
+       stickies = StickyNote.all
+       render json: stickies
+    end
+    def show
+       sticky = StickyNote.find(params[:id])
+       render json: sticky
+    end
+    def create
+       sticky = StickyNote.create(sticky_params)
+       render json: sticky
+    end
+    def update
+       sticky = StickyNote.find_by(id: params[:id])
+       sticky.update(sticky_params)
+       render json: sticky
+    end
+    def destroy
+       sticky = StickyNote.find_by(id: params[:id])
+       sticky.destroy
+    end
+    private
+    def sticky_params
+       params.require(:sticky_note).permit(:id, :note_category, :note_description)
+    end
+  end
--- back_end/db/seeds.rb
+++ back_end/db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+p "Initiating seeds..."
+StickyNote.create(
+   note_category: 'Mini-project',
+   note_description: 'CRUD on ugly notes'
+)
+StickyNote.create(
+   note_category: 'Project',
+   note_description: 'Style Me 3.0'
+)
+StickyNote.create(
+   note_category: 'Study',
+   note_description: 'More Algorithms'
+)
+p "Seeds planted"
